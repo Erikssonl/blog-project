9b8b96757fe7f25484d8d99f5c1097db49e734e6
Louise Eriksson <louiseriksson.92@gmail.com>
2023-12-11 15:33:27 +0100

    update js almoste done

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -1,4 +1,63 @@
 
+console.log('Query string: ', location.search)
+const urlParams = new URLSearchParams(location.search)
+console.log(urlParams.get('id'))
+
+fetchPost();
+
+async function fetchPost() {
+    try{
+        const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + urlParams.get('id'), );
+        if (!response.ok) {
+            throw new Error('Something when wrong');
+        }
+        const post = await response.json();
+        console.log(post);
+    
+    
+        document.getElementById('title').value = post.title;
+        document.getElementById('author').value = post.author;
+        document.getElementById('content-textarea').value = post.content;
+        // document.getElementById('tags').value = post.tags;
+
+    } catch(error) {
+        console.log(error)
+    }
+
+}
+
+document.getElementById('update-post-form').addEventListener('submit', updatePost)
+
+async function updatePost(e) {
+    e.preventDefault();
+    let form = e.target;
+
+    try {
+        let formData = new FormData(form)
+        let data = {
+            "title": formData.get('title'),
+            "author": formData.get('author'),
+            "content": formData.get('content')
+        };
+
+        let response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + urlParams.get('id'), {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(response.ok) {
+            location.replace('index.html');
+        }
+
+    } catch(error) {
+        console.log(error)
+    } 
+}
+
+
 const select = document.getElementById('tags');
 
 select.addEventListener('mousedown', (e) => {
--- blog-client-template/js/post.js
+++ blog-client-template/js/post.js
@@ -6,24 +6,30 @@ console.log(urlParams.get('id'))
 fetchPost();
 
 async function fetchPost() {
-    const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + urlParams.get('id'), );
-    if (!response.ok) {
-        throw new Error('Something when wrong');
+    try{
+        const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + urlParams.get('id'), );
+        if (!response.ok) {
+            throw new Error('Something when wrong');
+        }
+        const post = await response.json();
+    
+        let postDate = new Date(post.date)
+    
+        postHTML = `
+            <div>
+                <h1>${post.title}</h1>
+                <p>${post.author} <br> <span class="date"> ${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</span></p>
+                <p><span class="tags">tags: </span>${post.tags}</p> 
+                <div>${post.content}</div>
+                <br>
+                <a href="index.html" <i class="fa fa-long-arrow-left" aria-hidden="true"></i>  Back </a>
+            </div>
+        `
+    
+        document.getElementById('blog-post').innerHTML = postHTML;
+
+    } catch(error) {
+        console.log(error)
     }
-    const post = await response.json();
 
-    let postDate = new Date(post.date)
-
-    postHTML = `
-        <div>
-            <h1>${post.title}</h1>
-            <p>${post.author} <br> <span class="date"> ${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</span></p>
-            <p><span class="tags">tags: </span>${post.tags}</p> 
-            <div>${post.content}</div>
-            <br>
-            <a href="index.html" <i class="fa fa-long-arrow-left" aria-hidden="true"></i>  Back </a>
-        </div>
-    `
-
-    document.getElementById('blog-post').innerHTML = postHTML;
 }
