7f3e065e3fa139b0de8560c67cc86ebd0de09ba3
Louise Eriksson <louiseriksson.92@gmail.com>
2023-12-12 15:03:13 +0100

    delete button done and work

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -18,7 +18,7 @@ async function fetchAllBlogPosts(){
                 <td>${post.author}</td>
                 <td>${post.tags}</td>
                 <td>${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</td>
-                <td><a href="update-post.html?id=${post._id}">Update</a> | <a href="#">Delete</a></td>
+                <td><a href="update-post.html?id=${post._id}">Update</a> | <a href="#" data-id="${post._id}" class="delete-link">Delete</a></td>
             </tr>
             `
         }
@@ -28,4 +28,27 @@ async function fetchAllBlogPosts(){
     } catch(error) {
         console.log(error)
     }
+
+    let deletePostLinks = document.getElementsByClassName('delete-link')
+
+    for (let deleteLink of deletePostLinks){
+        deleteLink.addEventListener('click', async function(e){
+            e.preventDefault();
+            if (e.target.classname === 'delete-link') {
+
+            }
+            let postId = e.target.dataset.id;
+            let response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + postId, {
+                method: "DELETE"
+    
+            });
+
+            if(response.ok){
+                e.target.parentNode.parentNode.remove();
+                }
+
+
+        })
+    }
 }
+
