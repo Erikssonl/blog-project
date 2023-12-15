82b4c5273884e4360c8e824076c19d36ca74fa23
Louise Eriksson <louiseriksson.92@gmail.com>
2023-12-15 10:04:15 +0100

    solution to ignore empty posts

--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -12,16 +12,19 @@ async function fetchAllBlogPosts(){
         for (let post of posts){
             let postDate = new Date(post.date)
 
-            postsHTML += `
-            <li class="list-posts-item">
-                <h3>${post.title}</h3>
-                <p>${post.author} <br> <span class="date"> ${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</span></p>
-                <p><span class="tags">tags: </span>${post.tags}</p> 
-                <div>${post.content.substring(0, 100) + "...."} <a href="post.html?id=${post._id}">Read more</a> </div>
+            if (post.title && post.author && post.content) {
+                postsHTML += `
+                    <li class="list-posts-item">
+                        <h3>${post.title}</h3>
+                        <p>${post.author} <br> <span class="date"> ${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</span></p>
+                        <p><span class="tags">tags: </span>${post.tags}</p> 
+                        <div>${post.content.substring(0, 100) + "...."} <a href="post.html?id=${post._id}">Read more</a> </div>
+    
+                    </li>
+    
+                `
+            }
 
-            </li>
-
-            `
         }
 
         document.getElementById('blog-list').innerHTML = postsHTML;
